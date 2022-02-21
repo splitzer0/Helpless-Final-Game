580282dd12cfa712a3269f04e78f83b532ab862f
splitzer0 <56415162+splitzer0@users.noreply.github.com>
2022-02-21 11:26:07 -0800

    new commit

--- Assets/playerCameraController.cs
+++ Assets/playerCameraController.cs
@@ -12,12 +12,19 @@ public class playerCameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //limits mouse speed
+
+        MouseVector = Vector2.ClampMagnitude(MouseVector, 1);
+
+        MouseVector = MouseVector * 5;
+
         Vector2 ModifiedMouseVector = MouseVector * mouseSensitivity * Time.deltaTime;
 
         XRot -= ModifiedMouseVector.y;
