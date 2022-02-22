7b8ca647ea1c3a059d8a9a6b0936c313985663d3
splitzer0 <56415162+splitzer0@users.noreply.github.com>
2022-02-21 17:15:58 -0800

    fov

--- Assets/KillerAIEditor.cs
+++ Assets/KillerAIEditor.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+
 [CustomEditor(typeof(killerAI))]
-public class KillerAIEditor : Editor { 
-    private void OnScreenGUI()
+public class KillerAIEditor : Editor
+{
+    private void OnSceneGUI()
     {
         killerAI fov = (killerAI)target;
-        Handles.color = Color.white;
+        Handles.color = Color.blue;
         Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.radius);
 
         Vector3 viewAngle01 = DirectionFromAngle(fov.transform.eulerAngles.y, -fov.angle / 2);
         Vector3 viewAngle02 = DirectionFromAngle(fov.transform.eulerAngles.y, fov.angle / 2);
 
-        Handles.color = Color.yellow;
+        Handles.color = Color.red;
         Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle01 * fov.radius);
         Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle02 * fov.radius);
 
@@ -25,11 +27,10 @@ public class KillerAIEditor : Editor {
         }
     }
 
-    private Vector3 DirectionFromAngle(float eulerY, float angleInDegrees)
+    private Vector3 DirectionFromAngle(float EulerY, float angleInDegrees)
     {
-        angleInDegrees += eulerY;
+        angleInDegrees += EulerY;
 
         return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
     }
 }
-
