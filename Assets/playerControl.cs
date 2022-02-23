14afdc3195a303b5d9210dd7a9aab005ee1b2146
splitzer0 <56415162+splitzer0@users.noreply.github.com>
2022-02-22 22:04:27 -0800

    added Killers Hearing Code

--- Assets/playerControl.cs
+++ Assets/playerControl.cs
@@ -8,6 +8,13 @@ public class playerControl : MonoBehaviour
     public bool MovingCheck;
     public Vector2 RawMovementVectors;
 
+    public bool isSneaking;
+    public bool isSprinting;
+
+    public float sneakSpeed;
+    public float walkingSpeed;
+    public float runningSpeed;
+
     public float movementSpeed;
 
     public bool canInteract;
@@ -23,11 +30,21 @@ public class playerControl : MonoBehaviour
     void Update()
     {
 
-        Debug.Log(rb.velocity.magnitude);
+        //Debug.Log(rb.velocity.magnitude);
 
         if (rb.velocity.magnitude > 0.1)
         {
-            AudioEmitter.localScale = new Vector3(15, 15, 15);
+            if (isSprinting)
+            {
+                AudioEmitter.localScale = new Vector3(30, 30, 30);
+            }
+            else if(isSneaking && !isSprinting){
+                AudioEmitter.localScale = new Vector3(5, 5, 5);
+            }
+            else if(!isSneaking && !isSprinting)
+            {
+                AudioEmitter.localScale = new Vector3(15, 15, 15);
+            }
         }
         else if (rb.velocity.magnitude < 0.1)
         {
@@ -39,7 +56,7 @@ public class playerControl : MonoBehaviour
             MoveFunction();
         } else
         {
-            
+            return;
         }
 
         RaycastHit hit;
@@ -56,30 +73,28 @@ public class playerControl : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Enemy"))
-        {
-            other.gameObject.GetComponent<killerAI>().canHearPlayer = true;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.CompareTag("Enemy"))
-        {
-            other.gameObject.GetComponent<killerAI>().canHearPlayer = false;
-        }
-    }
-
     public void MoveFunction()
     {
+        if (isSprinting)
+        {
+            movementSpeed = runningSpeed;
+        }
+        if (isSneaking)
+        {
+            movementSpeed = sneakSpeed;
+        }
+        if (!isSprinting && !isSneaking)
+        {
+            movementSpeed = walkingSpeed;
+        }
+
         float forwardForce = RawMovementVectors.y * movementSpeed;
         float rightForce = RawMovementVectors.x * movementSpeed;
         rb.AddForce(transform.forward * forwardForce + transform.right * rightForce);
 
     }
 
+    //Controller Stuff
     public void PlayerMove(InputAction.CallbackContext context)
     {
         RawMovementVectors = context.ReadValue<Vector2>();
@@ -105,4 +120,47 @@ public class playerControl : MonoBehaviour
             }
         }
     }
+
+    public void sprint(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            isSprinting = true;
+        }
+        if (context.canceled)
+        {
+            isSprinting = false;
+        }
+    }
+
+    public void sneak(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            isSneaking = true;
+        }
+        if (context.canceled)
+        {
+            isSneaking = false;
+        }
+    }
+
+    //Triggers
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.GetComponent<killerAI>().canHearPlayer = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.GetComponent<killerAI>().canHearPlayer = false;
+        }
+    }
+
 }
